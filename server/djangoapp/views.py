a4dc235bb207e5a957f17ba4df5cc99ab18fec71
red <red.parker.red@gmail.com>
2024-07-09 09:43:27 -0400

    linting4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -145,7 +145,7 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as err:
             return JsonResponse({"status": 401, "message":
-                        "Error in posting review: {err}".format(err)})
+                                "Error in posting review: {}".format(err)})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 # ...
